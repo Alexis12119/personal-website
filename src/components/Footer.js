b5f95ebe4d1b671d750e02cb6082a8c52aae5624
Alexis12119 <corporal461@gmail.com>
2024-07-10 08:50:03 +0800

    feat(links): open in new tab.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
     <footer className="bg-gray-800 dark:bg-gray-200 text-white dark:text-black p-4 text-center">
       <p>&copy; 2024 My Portfolio. All rights reserved.</p>
       <div className="flex justify-center space-x-4">
-        <a href="https://twitter.com" className="hover:text-gray-400"><FaTwitter /></a>
-        <a href="https://linkedin.com" className="hover:text-gray-400"><FaLinkedin /></a>
-        <a href="https://github.com" className="hover:text-gray-400"><FaGithub /></a>
+        <a href="https://twitter.com" target="_blank" className="hover:text-gray-400"><FaTwitter /></a>
+        <a href="https://linkedin.com" target="_blank" className="hover:text-gray-400"><FaLinkedin /></a>
+        <a href="https://github.com/Alexis12119" target="_blank" className="hover:text-gray-400"><FaGithub /></a>
       </div>
     </footer>
   );
